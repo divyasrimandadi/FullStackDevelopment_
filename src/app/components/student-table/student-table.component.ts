e9b3b9b14e0337231dbcc39a6d1ff0b54fd54c6a
Divya Sri <divyasrimandadi8@gmail.com>
2025-02-02 00:16:56 +0530

    done

--- src/app/components/student-table/student-table.component.ts
+++ src/app/components/student-table/student-table.component.ts
@@ -3,14 +3,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-student-table',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './student-table.component.html',
   styleUrl: './student-table.component.css'
 })
 export class StudentTableComponent {
   studentData=[
-    {Id:1, Student : 'Divya',Phone:9014356789, Email :'divya@gmail.com', Gender:'Female',Marks:95},
-    {Id:2, Student : 'Chandana',Phone:9182515757, Email :'chandana@gmail.com', Gender:'Female',Marks:90}
+    {Id:1, Student : 'Divya',Phone:9059667087, Email :'divya@gmail.com', Gender:'Female',Marks:95},
+    {Id:2, Student : 'Jaya',Phone:9121540158, Email :'jaya@gmail.com', Gender:'Female',Marks:90},
+    {Id:3, Student : 'Bhujanga Rao',Phone:9908940158, Email :'bhujangarao@gmail.com', Gender:'Male',Marks:100},
+    {Id:4, Student : 'Usha',Phone:9618801508, Email :'usha@gmail.com', Gender:'Female',Marks:99},
+    
+    
   ]
   
   
