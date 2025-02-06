e9b09832cdb9a6f0441755a8664638fc57789753
Divya Sri <divyasrimandadi8@gmail.com>
2025-02-06 18:39:33 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,15 @@ import { AddStudentComponent } from './components/add-student/add-student.compon
 })
 export class AppComponent {
   title=''
-  handleAddStudent(event:any){
-    console.log(event);
-  }
+ 
   //data stays on parent
   //parent pass the data to child
-  studentData=[
-    {Id:1, Student : 'Divya',Phone:9059667087, Email :'divya@gmail.com', Gender:'Female',Marks:95},  
+  studentData:any=[
+      
   ]
+  handleAddStudent(event:any){
+    console.log('data from parent',event);
+    this.studentData.push(event)
+  }
   
 }
